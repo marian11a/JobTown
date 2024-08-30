421d10123a908cd6c713d071898027a4d139b2ac
marian11a <137102580+marian11a@users.noreply.github.com>
2024-08-30 15:05:35 +0300

    repo init

---
+++ src/main/java/com/example/JobTown/JobTownApplication.java
@@ -0,0 +1,13 @@
+package com.example.JobTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JobTown/JobTownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JobTown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
